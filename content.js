d244a9d1ccee28935bc4bd30a9d91324a5cec722
Chen Wang <lukeshining@gmail.com>
2017-05-15 16:08:45 -0400

    Updated baiduNetDist regex

--- content.js
+++ content.js
@@ -47,7 +47,7 @@ function replacer(){
     chrome.storage.local.get("baidu", function(value){
         if (value.baidu == "true") {
             //Regex definitions
-            var baiduPattern = /[\s\/^:\uff1a](?=.{0,7}[a-z])(?=.{0,7}[0-9])([0-9a-z]{8})(?=$|[^0-9a-z.='"])\s*(\u5bc6\u7801:?)?([0-9a-z]{4})?/gi;
+            var baiduPattern = /[\s\/^:\uff1a](?=.{0,7}[a-z])(?=.{0,7}[0-9])(1[0-9a-z]{7})(?=$|[^0-9a-z.='"])\s*(\u5bc6\u7801:?)?\s{0,2}([0-9a-z]{4})?/gi;
             //Replacer functions
             function baiduReplacer(match, p1){
                 //p10 the number ID
